ab9e63adfd4b0da1919d598b348ae13b34e88d19
cortelucas <corte1994.lc@gmail.com>
2021-03-03 15:18:56 -0300

    - implementação do metodo sacar

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 class Cliente {
     nome;
     cpf;
-    rg;
+    rg;    
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor) {
+            this.sacar -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
@@ -11,17 +20,28 @@ const cliente1 = new Cliente();
 cliente1.nome = 'Lucas';
 cliente1.cpf = '12345678900';
 cliente1.rg = '11222330';
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
-console.log(cliente1);
+const contaCliente1 = new ContaCorrente();
+contaCliente1.agencia = 1001;
+contaCliente1.saldo = 0;
+
+console.log(
+    cliente1, +'\n'+
+    contaCliente1
+);
+
 
 const cliente2 = new Cliente();
 
 cliente2.nome = 'Andressa';
 cliente2.cpf = '98765432100';
 cliente1.rg = '33222110';
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1);
+const contaCliente2 = new ContaCorrente();
+contaCliente2.agencia = 1001;
+contaCliente2.saldo = 0;
+
+console.log(
+    cliente2, +'\n'+
+    contaCliente2
+);
